dbc95645f2e8369ab65ad13dafd0bace12d54949
3jfmin <185140062+3jfmin@users.noreply.github.com>
2024-11-29 11:07:26 +0900

    Create ChatMessage.js

---
+++ components/ChatMessage.js
@@ -0,0 +1,8 @@
+export default function ChatMessage({ message }) {
+  return (
+    <div className="message">
+      <p>{message.text}</p>
+      <span>{new Date(message.timestamp.seconds * 1000).toLocaleTimeString()}</span>
+    </div>
+  );
+}
